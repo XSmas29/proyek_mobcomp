a9bfbfaee5af777b618280825b3de1e46c234845
Hans Felix <hansfeljul2001@gmail.com>
2021-11-27 21:28:56 +0700

    tambah file fragment dan activity java

---
+++ app/src/main/java/com/example/proyek_mobcomp/ConfTopUpDetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.proyek_mobcomp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ConfTopUpDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conf_top_up_detail);
+    }
+}
