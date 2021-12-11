add64abee8ec75cd9861f33ba00afa1501ed449b
Surya Bumantara <bumantara.surya2213@gmail.com>
2021-12-11 19:10:03 +0700

    design recyclerview search grid layout

--- app/src/main/java/com/example/proyek_mobcomp/CustomerSearchActivity.java
+++ app/src/main/java/com/example/proyek_mobcomp/CustomerSearchActivity.java
@@ -2,6 +2,8 @@ package com.example.proyek_mobcomp;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -15,8 +17,11 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.proyek_mobcomp.classFolder.cProduct;
+import com.example.proyek_mobcomp.classFolder.cUser;
 import com.example.proyek_mobcomp.databinding.ActivityCustomerHomeBinding;
 import com.example.proyek_mobcomp.databinding.ActivityCustomerSearchBinding;
+import com.example.proyek_mobcomp.recyclerviewFolder.RecyclerAdapterCustomerHomeProduct;
+import com.example.proyek_mobcomp.recyclerviewFolder.RecyclerAdapterCustomerSearchProduct;
 import com.squareup.picasso.Picasso;
 import com.synnapps.carouselview.ImageClickListener;
 import com.synnapps.carouselview.ImageListener;
@@ -25,12 +30,16 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class CustomerSearchActivity extends AppCompatActivity {
 
     ActivityCustomerSearchBinding binding;
+    ArrayList<cProduct> listSearch = new ArrayList<>();
+    ArrayList<String[]> listowner = new ArrayList<>();
+    RecyclerAdapterCustomerSearchProduct recyclerAdapterCustomerSearchProduct;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,8 +53,6 @@ public class CustomerSearchActivity extends AppCompatActivity {
             binding.txtKeyword.setText("Search Result for '" + getIntent().getStringExtra("keyword") + "'");
             loadSearch();
         }
-
-
     }
 
     private void loadSearch() {
@@ -59,8 +66,31 @@ public class CustomerSearchActivity extends AppCompatActivity {
                         try {
                             JSONObject jsonObject = new JSONObject(response);
                             System.out.println(jsonObject);
-                            JSONArray arraycarousel = jsonObject.getJSONArray("databarang");
+                            JSONArray arraysearch = jsonObject.getJSONArray("databarang");
 
+                            for (int i = 0; i < arraysearch.length(); i++){
+                                int id = arraysearch.getJSONObject(i).getInt("id");
+                                String fk_seller = arraysearch.getJSONObject(i).getString("fk_seller");
+                                int fk_kategori = arraysearch.getJSONObject(i).getInt("fk_kategori");
+                                String nama = arraysearch.getJSONObject(i).getString("nama");
+                                String deskripsi = arraysearch.getJSONObject(i).getString("deskripsi");
+                                int harga = arraysearch.getJSONObject(i).getInt("harga");
+                                int stok = arraysearch.getJSONObject(i).getInt("stok");
+                                String gambar = arraysearch.getJSONObject(i).getString("gambar");
+                                int is_deleted = arraysearch.getJSONObject(i).getInt("is_deleted");
+
+                                //data toko
+                                JSONObject dataowner = arraysearch.getJSONObject(i).getJSONObject("owner");
+                                String namatoko = dataowner.getString("toko");
+                                String gambartoko = dataowner.getString("gambar");
+
+                                listSearch.add(
+                                        new cProduct(id, fk_seller, fk_kategori, nama, deskripsi, harga, stok, gambar, is_deleted)
+                                );
+
+                                listowner.add(new String[] {namatoko, gambartoko});
+                            }
+                            setRvSearchGrid();
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
@@ -84,5 +114,14 @@ public class CustomerSearchActivity extends AppCompatActivity {
 
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         requestQueue.add(stringRequest);
+    }
+
+    private void setRvSearchGrid() {
+        binding.rvSearch.setLayoutManager(new GridLayoutManager(this, 2));
+        binding.rvSearch.setHasFixedSize(true);
+
+        recyclerAdapterCustomerSearchProduct = new RecyclerAdapterCustomerSearchProduct(listSearch, listowner);
+        binding.rvSearch.setAdapter(recyclerAdapterCustomerSearchProduct);
+
     }
 }
